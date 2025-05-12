7bd1a8402e1f96b7d7efb370efca6ee2a03bf975
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-05-12 09:35:48 +0530

    login is added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import * as userService from '../services/user.service.js';
 import { validationResult } from 'express-validator';
+import userModel from '../models/user.model.js';
 
 export const createUserController = async (req, res) => {
    const errors = validationResult(req);
@@ -14,4 +15,40 @@ export const createUserController = async (req, res) => {
    } catch (error) {
       res.status(400).json({ status: "the error is geted", message: error.message });
    }
+}
+
+export const loginController = async (req, res) => {
+
+   const errors = validationResult(req);
+
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ message: "Invalid Email Or Password" });
+   }
+
+   try {
+      const { email, password } = req.body;
+      console.log(email,password)
+      const user = await userModel.findOne({ email }).select('+password');
+      console.log(user)
+
+      if (!user) {
+         return res.status(400).json({message:"User Not Found",error:"Invalid userid or password"})
+      }
+      const isMatch=await user.isValidPassword(password);
+      console.log(isMatch)
+      if(!isMatch){
+         return res.status(400).json({message:"user not found", error:"invlid userid or password"})
+      }
+      const token=await user.generateToken();
+      res.status(200).json({user,token})
+
+   } catch (err) {
+      console.log(err)
+      res.status(400).json(err.message)
+   }
+
+}
+
+export const profileController=async (req,res)=>{
+   
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,20 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import * as userController from '../controllers/user.controller.js';
-import {body} from 'express-validator';
+import { body } from 'express-validator';
 
-const router=Router();
+const router = Router();
 
 router.post('/register',
-    // body('email').isEmail().withMessage('email must be a valid email address'),
-    // body('password').isLength({min:3}).withMessage('password must be atleast length of 3'), 
+    body('email').isEmail().withMessage('email must be a valid email address'),
+    body('password').isLength({ min: 3 }).withMessage('password must be atleast length of 3'),
     userController.createUserController);
+
+router.post('/login',
+    body('email').isEmail().withMessage('email must be a valid email address'),
+    body('password').isLength({ min: 3 }).withMessage('password must be atleast length of 3'),
+    userController.loginController
+)
+
+router.get('/profile',userController.profileController)
 
 export default router;
