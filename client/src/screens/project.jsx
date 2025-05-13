6480b6d5f9d57a41859e6e09fb973f8d6ac8dda8
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-05-13 13:27:04 +0530

    on working on the adding user

--- client/src/screens/project.jsx
+++ client/src/screens/project.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 
 const project = () => {
     const location = useLocation();
     const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
     console.log(location.state)
     return (
         <main className='h-screen w-full flex bg-white'>
             <section className='flex flex-col left h-full min-w-96 bg-slate-200'>
-                <header className='flex justify-end px-4 py-3 bg-slate-100'>
+                <header className='flex justify-between px-4 py-3 bg-slate-100'>
+                    <button className='p-2 cursor-pointer flex gap-2 items-center'
+                        onClick={() => setIsOpenModal(!isOpenModal)}>
+                        <i className='ri-add-fill'></i>
+                        <p className='text-sm'>Add User</p>
+                    </button>
                     <button className='p-2 cursor-pointer ' onClick={() => setIsPanelOpen(!isPanelOpen)}>
                         <i className="ri-group-fill"></i>
                     </button>
@@ -43,9 +55,57 @@ const project = () => {
                             <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
                             <h1 className='text-[18px]'>name of user</h1>
                         </div>
-                    </div>  
+                    </div>
                 </div>
-            </section>  
+            </section>
+            {isOpenModal && (
+                <div className="fixed inset-0 flex items-center justify-center">
+                    <div className="flex flex-col h-96 w-96 bg-slate-100">
+                        <header className='flex justify-between px-4 py-3 bg-slate-300'>
+                            <div></div>
+                            <h1>Add Users</h1>
+                            <button className='cursor-pointer ' onClick={() => setIsOpenModal(!isOpenModal)}>
+                                <i className="ri-close-line"></i>
+                            </button>
+                        </header>
+                        <div className='flex-grow relative flex flex-col overflow-auto'>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                            <div className='flex gap-4 items-center p-2 cursor-pointer hover:bg-slate-300'>
+                                <div className='flex justify-center items-center w-fit h-fit p-6 bg-slate-500 rounded-[100%]'><i className="ri-user-fill absolute"></i></div>
+                                <h1 className='text-[18px]'>name of user</h1>
+                            </div>
+                        </div>
+                        <div className='h-4 w-full'>
+                            <button className='cursor-pointer px-4 py-2 w-full bg-blue-500'>Add Colleeborator</button>
+                        </div>
+                    </div>
+                </div>
+
+            )}
+
         </main>
     )
 }
