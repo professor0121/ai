42706a4567f4c497838470dfe874847e13e35b93
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-05-14 10:18:41 +0530

    client is conned to socket

--- server/server.js
+++ server/server.js
@@ -2,37 +2,49 @@ import dotenv from 'dotenv';
 dotenv.config();
 import http from 'http';
 import app from './app.js';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin:"*",
+        methods: ['GET', 'POST'],
+
+  }
+});
 
 //Socket Middleware for authenticating Socket User...
-io.use(async(socket,next)=>{
-  const token=socket.handshake.auth?.token||socket.handshake.headers.authorization?.split(' ')[1];
-  if(!token){
+io.use( (socket, next) => {
+  try {
+    const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+  if (!token) {
     return next(new Error("Authentication Error"))
   }
-  const decoded=jwt.verify(token,process.env.JWT_SECRET)
+  const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-  if(!decoded){
+  if (!decoded) {
     return next(new Error("Authentication Error"))
   }
 
-  socket.user=decoded;
+  socket.user = decoded;
+    next();
+  } catch (error) {
+console.log(error)
+  }
 })
 
 
 
 io.on('connection', socket => {
-    console.log("User Is Connected")
-  socket.on('event', data => { /* … */ });
-  socket.on('disconnect', () => { /* … */ });
+  console.log("User Is Connected")
+  // socket.on('event', data => { /* … */ });
+  // socket.on('disconnect', () => { /* … */ });
 });
 
-server.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 })
