1cd518b6f6c04fd7f0fb557c593bebb02900ee40
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-05-14 09:11:19 +0530

    on going socket servr

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import http from 'http';
 import app from './app.js';
+import {Server} from 'socket.io'
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+
+const io = new Server(server);
+io.on('connection', socket => {
+    console.log("User Is Connected")
+  socket.on('event', data => { /* … */ });
+  socket.on('disconnect', () => { /* … */ });
+});
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
